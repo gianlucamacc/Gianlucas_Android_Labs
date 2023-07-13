475e0290eed39cb0e8d3791cea6cf39e4a4a6d90
Gianluca Macchiagodena <gianlucamacchia13@gmail.com>
2023-07-12 22:52:33 -0400

    Lab 6. just added the receive button. Still not finished, having errors.

--- app/src/main/java/algonquin/cst2335/macc0112/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/macc0112/ChatRoom.java
@@ -1,19 +1,89 @@
 package algonquin.cst2335.macc0112;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import algonquin.cst2335.macc0112.databinding.ActivityChatRoomBinding;
+import algonquin.cst2335.macc0112.databinding.SentMessageBinding;
+import algonquin.cst2335.macc0112.ui.data.ChatRoomViewModel;
+
 
 public class ChatRoom extends AppCompatActivity {
 
     ActivityChatRoomBinding binding;
+    ChatRoomViewModel chatModel ;
+    ArrayList<String> messages = new ArrayList<>();
+    private RecyclerView.Adapter myAdapter;
+
+
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView message;
+        TextView time;
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.message);
+            time = itemView.findViewById(R.id.time);
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
+        messages = chatModel.messages.getValue();
+        if(messages == null)
+            {
+            chatModel.messages.postValue( messages = new ArrayList<String>());
+            }
 
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_chat_room);
+
+
+
+
+        myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+        @NonNull
+        @Override
+        public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+            return new MyRowHolder(binding.getRoot());
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+            String obj = messages.get(position);
+            holder.message.setText(obj);
+            holder.time.setText("");
+        }
+
+        @Override
+        public int getItemCount() {
+            return messages.size();
+        }
+        @Override
+        public int getItemViewType(int position){
+            return 0;
+        }
+    };
+        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.sendButton.setOnClickListener(click -> {
+            messages.add(binding.textInput.getText().toString());
+            myAdapter.notifyItemInserted(messages.size()-1);
+
+            binding.textInput.setText("");
+        });
+
     }
 }
--- app/src/main/java/algonquin/cst2335/macc0112/ui/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/macc0112/ui/ui/MainActivity.java
@@ -1,68 +1,30 @@
-package algonquin.cst2335.macc0112.ui.ui;
+package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 
 import algonquin.cst2335.macc0112.databinding.ActivityMainBinding;
 import algonquin.cst2335.macc0112.ui.data.MainViewModel;
 
+
 public class MainActivity extends AppCompatActivity {
-    private MainViewModel model;
     private ActivityMainBinding variableBinding;
-
-
+    private MainViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         model = new ViewModelProvider(this).get(MainViewModel.class);
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-        });
-        model.editString.observe(this, s -> {
-            variableBinding.textview.setText("Your edit text has: " + s);
-        });
 
-        model.isSelected.observe(this, selected -> {
-            variableBinding.checkBox.setChecked(selected);
-            variableBinding.radioButton.setChecked(selected);
-            variableBinding.switch1.setChecked(selected);
 
-            String message = "The value is now: " + selected;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-                });
-        variableBinding.checkBox.setOnCheckedChangeListener((checkBox, isChecked)-> {
-            model.isSelected.postValue(variableBinding.checkBox.isChecked());
-        });
 
-        variableBinding.radioButton.setOnCheckedChangeListener((radioButton, isChecked)-> {
-            model.isSelected.postValue(variableBinding.radioButton.isChecked());
-        });
-
-        variableBinding.switch1.setOnCheckedChangeListener((switch1, isChecked)-> {
-            model.isSelected.postValue(variableBinding.switch1.isChecked());
-        });
-
-        variableBinding.imageView.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "ImageView clicked", Toast.LENGTH_SHORT).show();
-        });
-
-        variableBinding.myimagebutton.setOnClickListener(view -> {
-            int width = view.getWidth();
-            int height = view.getHeight();
-            String message = "The width = " + width + " and height = " + height;
-            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-        });
     }
-
 }
