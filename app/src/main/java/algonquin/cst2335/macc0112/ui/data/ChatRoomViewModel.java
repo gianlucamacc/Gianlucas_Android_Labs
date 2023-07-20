4f472485ef557547d081dac7669e2552e2c57d21
Gianluca Macchiagodena <gianlucamacchia13@gmail.com>
2023-07-19 20:16:43 -0400

    Lab 7: finished

--- app/src/main/java/algonquin/cst2335/macc0112/ui/data/ChatRoomViewModel.java
+++ app/src/main/java/algonquin/cst2335/macc0112/ui/data/ChatRoomViewModel.java
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
-import algonquin.cst2335.macc0112.ChatRoom.ChatMessage;
+
 public class ChatRoomViewModel extends ViewModel {
     public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData<>();
 }
